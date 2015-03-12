97292cd9904f531b815aab7816e880ead8718241
likhachev <likhachev@wks584.elektron.spb.su>
2015-03-13 02:45:27 +0400

    1

--- src/com/ivli/roim/LutPanel.java
+++ src/com/ivli/roim/LutPanel.java
@@ -19,57 +19,53 @@ import javax.swing.JComponent;
  * @author likhachev
  */
 public class LutPanel extends JComponent implements WindowChangeListener {
-    
-   JMedImagePane    iPanel = null; 
-   BufferedImage iBuf = null;
- 
-   private LutPanel(){}
-   public LutPanel(JMedImagePane aP) {iPanel = aP;}
-           
-           
-   public void setSize(Dimension d) {
-       super.setSize(d);
-       init2();
-   }
+    final JMedImagePane iPanel; 
+    BufferedImage iBuf;
+
+    public LutPanel(JMedImagePane aP) {iPanel = aP;}
+       
+    public void setSize(Dimension d) {
+        super.setSize(d);
+        init2();
+    }
     void init2() {
-        int w = getWidth();
-        int h = getHeight();
-        ComponentColorModel cm=new ComponentColorModel(
+        final int w = getWidth();
+        final int h = getHeight();
+        final ComponentColorModel cm = new ComponentColorModel(
 			ColorSpace.getInstance(ColorSpace.CS_GRAY),
 			new int[] {8},
 			false,		// has alpha
 			false,		// alpha premultipled
 			Transparency.OPAQUE,
-			DataBuffer.TYPE_USHORT
-		);
+			DataBuffer.TYPE_USHORT);
+		
+        final ComponentSampleModel sm = new ComponentSampleModel(
+                DataBuffer.TYPE_USHORT,
+                w,
+                h,
+                1,
+                w,
+                new int[] {0});
+		         
+        final double delta = (iPanel.getMaximum() - iPanel.getMinimum())/getHeight();
+        final int stepy = getHeight() / 255;
+        short data [] = new short[w*h];
 
-		ComponentSampleModel sm = new ComponentSampleModel(
-			DataBuffer.TYPE_USHORT,
-			w,
-			h,
-			1,
-			w,
-			new int[] {0}
-		);
-                
-                final double delta = (iPanel.getMaximum() - iPanel.getMinimum())/getHeight();
-                final int stepy = getHeight() / 255;
-                short data [] = new short[w*h];
-                
-                for (int i = 0; i < h;) {
-                    for (int m = 0; m < stepy; ++i, ++m)
-                    for (int j = 0; j < w; ++j)
-                        data[i*w+j] =(short)((h-i)*delta);
-                }
-                
-		DataBuffer buf = new DataBufferUShort(data, w, 0);
-                
-		WritableRaster wr = Raster.createWritableRaster(sm,buf,new Point(0,0));
-                
-		iBuf = new BufferedImage(cm,wr,true,null);	// no properties hash table
+        for (int i = 0; i < h;) {
+            for (int m = 0; m < stepy; ++i, ++m)
+            for (int j = 0; j < w; ++j)
+                data[i*w+j] =(short)((h-i)*delta);
+        }
+
+        DataBuffer buf = new DataBufferUShort(data, w, 0);
+
+        WritableRaster wr = Raster.createWritableRaster(sm, buf, new Point(0,0));
+
+        iBuf = new BufferedImage(cm, wr, true, null);	// no properties hash table
     }
+    
     public void windowChanged(WindowChangeEvent anEvt) {
-            repaint();
+        repaint();
     }    
 
     public void paintComponent(Graphics g) {  
